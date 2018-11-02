25a67712a4995604e1bb5888f1802c2aa1f7397d
Mike Choi <mike.choi@live.ca>
2018-11-02 14:30:39 -0700

    Added axios default configurations

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,14 +10,14 @@ class FullPost extends Component {
     componentDidUpdate() {
         if(this.props.id){
             if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                 .then(res => this.setState({ loadedPost: res.data }));           
             }
         }
     }
 
     deletePostHandler = () => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.id)
         .then(res => {
             console.log(res);
         });
