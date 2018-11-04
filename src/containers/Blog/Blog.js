8b6f35366fe091b55aa288cb461f0766674a76e6
Mike Choi <mike.choi@live.ca>
2018-11-04 02:15:49 -0800

    Added switch component to only render a single page at a time

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
@@ -31,9 +31,11 @@ class Blog extends Component {
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <Posts />} /> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new" exact component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new" exact component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
