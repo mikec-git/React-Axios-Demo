0a5fa0ce4da3ba34528c35a7b0103b643af81d06
Mike Choi <mike.choi@live.ca>
2018-11-04 02:07:58 -0800

    Added route paths for other pages and added active class name and styling for navlinks

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import { Route, NavLink } from 'react-router-dom';
+import FullPost from './FullPost/FullPost';
+import './Blog.css';
 
 class Blog extends Component {
   render () {
@@ -9,12 +12,28 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li><NavLink 
+                to="/" 
+                exact
+                activeClassName="my-active"
+                activeStyle={{
+                  color: "#fa923f",
+                  textDecoration: 'underline'
+                }}>Home</NavLink></li>
+              <li><NavLink 
+                to={{
+                  pathname: '/new',
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                }} 
+                exact>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
-        <Posts />
+        {/* <Route path="/" exact render={() => <Posts />} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new" exact component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
+import { Link } from 'react-router-dom';
 import './Posts.css';
 
 class Posts extends Component {
@@ -9,6 +10,8 @@ class Posts extends Component {
   }
   
   componentDidMount() {
+    console.log(this.props);
+
     axios.get('/posts')
       .then(res => {
         const posts = res.data.slice(0, 4);
@@ -29,11 +32,16 @@ class Posts extends Component {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
       if(!this.state.error){
         posts = this.state.posts.map(post => {
-          return <Post 
-            key={post.id} 
-            title={post.title} 
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>
+          return (
+            <Link 
+              to={'/' + post.id}
+              key={post.id} >
+              <Post 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
+            </Link>
+          );
         });
       }
 
